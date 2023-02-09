1a89c6f31e63f817a9cd384ae5d547c73b2a33c7
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-02-09 14:00:49 +0530

    learning about lexer

---
+++ 2-lexer/main.go
@@ -0,0 +1,28 @@
+// Lexer -> It simply understands the grammer of the language and it checks the statement that is written is correct or not .
+// it is done before the program is run by the compiler
+// its check whether we are following the rules or not
+// sometimes we dont need to put semicolons at some points because lexer go through that statement and will automatically puts semicolon over there
+
+/*
+types
+
+	it is case sensitive,
+	variable type should be known in advance
+	Everything is a Type
+
+
+	String, Bool, Integer( uint8, uint64, int8, int64, uintptr), Floating( float32, float64), Complex
+
+	alias are involved too
+	iota exists too
+	Functions , Chennels and Mutex
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World");
+}
