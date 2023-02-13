1ddd1974ef83696cc7003f8970a3f84720a6ba35
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-02-13 10:26:07 +0530

    memory management in go lang

---
+++ 8-memory-management/main.go
@@ -0,0 +1,16 @@
+// memoery management
+/*
+	new()
+-> allocate memory but not INIT
+-> you get memory address
+-> zeroed storage
+
+	make()
+-> allocate memory and INIT
+-> you will get a memory address
+-> non-zeroed storage
+
+garbage collector happens automatically
+anything which is out of scope or nil is eligible for garbage collector
+
+*/
