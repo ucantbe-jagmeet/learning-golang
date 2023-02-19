0627e7659b798586239edea5e3c92404c27c6cb8
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-02-19 16:43:36 +0530

    learning files

--- 20-files-in-golang/main.go
+++ 20-files-in-golang/main.go
@@ -1,7 +1,49 @@
+/*
+
+ */
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
-	fmt.Println("Working on Files in go lang")
+	fmt.Println("Welcome to Files in go lang")
+
+	content := "This needs to go in a file - LearnCodeOnline.in"
+
+	file, err := os.Create("./mygofile.txt")  // os is used to create the file
+
+	checkNilErr(err)
+
+
+	length, err := io.WriteString(file, content) // io is used to write anything
+	checkNilErr(err)
+
+
+	fmt.Println("Length is:",length)
+	defer file.Close()
+
+	readFile("./mygofile.txt")
+
+
+}
+
+func readFile(fileName string){
+	databyte, err := ioutil.ReadFile(fileName)     // ioutil is used to read the file
+	checkNilErr(err)
+
+	fmt.Println("text data inside the file is \n",string(databyte))
+
+}
+
+
+func checkNilErr(err error){
+	if err!=nil{
+		panic(err) // panic will shut down the program and throw whatever error we get
+	}
 }
