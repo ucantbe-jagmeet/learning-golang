0de0e898cb9d5c754048c24def8ffd83ccafcc74
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-02-24 21:33:13 +0530

    learning get one course

--- hitesh-choudhary/30-build-api/main.go
+++ hitesh-choudhary/30-build-api/main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+// Model for course - file
+type Course struct {
+	CourseId string `json:"courseid"`
+	CourseName string `json:"coursename"`
+	CoursePrice int `json:"price"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website string `json:"website"`
+}
+
+//fake DB 
+var courses []Course
+
+// middleware, helper - file
+func (c *Course) IsEmpty() bool{
+	return c.CourseId == "" && c.CourseName ==""
+}
+
 func main() {
 	fmt.Println("Building API in go lang")
 }
---
+++ hitesh-choudhary/32-get-course/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Model for course - file
+type Course struct {
+	CourseId string `json:"courseid"`
+	CourseName string `json:"coursename"`
+	CoursePrice int `json:"price"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website string `json:"website"`
+}
+
+//fake DB 
+var courses []Course
+
+// middleware, helper - file
+func (c *Course) IsEmpty() bool{
+	return c.CourseId == "" && c.CourseName ==""
+}
+
+func main() {
+	fmt.Println("Building API in go lang")
+}
+
+//controllers - file
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1> Welcome to Api by LearnCodeOnline</h1>"))
+}
+
+
+func getAllcourses( w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+
+func getOneCourse( w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get one courses")
+	w.Header().Set("Content-Type", "application/json")
+
+	//grab id from request
+	params := mux.Vars(r)
+
+	// loop through courses, find matching if and return the response
+	for _, course := range courses{
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No Course found wiht given id")
+	return
+}
